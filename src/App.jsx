ee75f481fd9adbb04654b001946582770906e906
Andres Berdugo <berrugo@gmail.com>
2025-12-30 07:28:11 -0500

    Bump, fix speed insights

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Footer from "./components/Footer";
 import FileUploader from "./components/FileUploader";
 import ThumbnailGallery from "./components/ThumbnailGallery";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import "./App.css";
 
 try {
